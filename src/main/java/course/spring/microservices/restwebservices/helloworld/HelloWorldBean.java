626975566c863b3569c6182444ac603df4f1de3d
Escarsel <eloisa.scarsella@everis.com>
2022-08-24 16:42:59 +0200

    First controller

---
+++ src/main/java/course/spring/microservices/restwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,19 @@
+package course.spring.microservices.restwebservices.helloworld;
+
+public class HelloWorldBean {
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+}
