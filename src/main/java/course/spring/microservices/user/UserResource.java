3fdbbd8f3dcc528a743ee451cfce4a2d2c9ddead
Escarsel <eloisa.scarsella@everis.com>
2022-08-25 09:58:01 +0200

    Create user

---
+++ src/main/java/course/spring/microservices/user/UserResource.java
@@ -0,0 +1,31 @@
+package course.spring.microservices.user;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return service.findOne(id);
+    }
+
+    @PostMapping("/create")
+    public void createUser(@RequestBody User user){
+        service.save(user);
+    }
+
+}
